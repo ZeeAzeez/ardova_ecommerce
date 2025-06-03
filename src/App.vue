a856ff79a9644f62e66896a989c42887d7c6aafb
ZeeAzeez <a.abdulazeez@ardovaplc.com>
2025-06-03 23:38:37 +0100

    sub hero

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <Header />
+    <Hero />
+    <Products />
   </div>
 </template>
+
 <script setup>
 import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import Products from "./components/Products.vue";
 </script>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,42 @@
-<template lang="">
-  <div></div>
+<template>
+  <section id="home" class="bg-amber-50 scroll-mt-20 py-16">
+    <div
+      class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col reverse lg:flex-row items-center gap-10"
+    >
+      <!-- left content -->
+      <div class="text-center lg:text-left max-w-xl">
+        <h1
+          class="text-4xl sm:text-5xl font-bold text-amber-950 leading-tight mb-4"
+        >
+          Powering the Future with Sustainable Energy
+        </h1>
+        <p class="text-gray-700 text-base sm:text-lg mb-6">
+          Explore our range of high-quality oil and gas products designed for
+          efficiency and environmental responsibility.
+        </p>
+        <div class="flex justify-center lg:justify-start gap-4">
+          <a
+            href="/products"
+            class="px-6 py-3 bg-pink-900 text-white text-sm font-semibold rounded-lg shadow hover:bg-pink-600 transition"
+            >Shop Now</a
+          >
+
+          <a
+            href="/offers"
+            class="px-6 py-3 border border-pink-500 text-pink-600 text-sm font-semibold rounded-lg hover:bg-pink-200 transition"
+            >View Offers</a
+          >
+        </div>
+      </div>
+
+      <!-- Right side -->
+      <div class="w-full lg:w-1/2 flex justify-center">
+        <img
+          src="../assets/images/hero.png"
+          alt="Hero Product"
+          class="w-3/4 lg:w-full max-w-md mx-auto rounded-xl shadow-lg"
+        />
+      </div>
+    </div>
+  </section>
 </template>
-<script>
-export default {};
-</script>
-<style lang=""></style>
